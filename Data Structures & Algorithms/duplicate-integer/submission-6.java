588cffa77dbf67b9a2908ffac32c3678b5ca02a1
digang <76679476+digang777@users.noreply.github.com>
2026-05-28 21:38:00 +0400

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        return map.entrySet().stream().anyMatch(e -> e.getValue() > 1);
+    }
+}
